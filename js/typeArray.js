8ed01e250a6a95225ba85b5e7684535f7bc71525
Gustavo Garcia <gustavogblanco@live.com>
2021-10-06 18:26:07 -0500

    type array

---
+++ js/typeArray.js
@@ -0,0 +1,16 @@
+//Sintaxis -> var nombreArray = []
+//var nombreArray = [valor1, valor2, valor3];
+var arreglo = [1,2,3,4,5,6,7,8,9];
+
+//Como le pido una posicion a un arreglo
+//sintaxis -> nombreArray[posicion]
+console.log(arreglo[6]);
+
+//como le pido el tamaño a un arreglo
+//Sintaxis -> nombreArray.length
+console.log(arreglo.length);
+
+//Como se le pide la ultima posicion de una 
+//arreglo
+console.log(arreglo[arreglo.length - 1]);
+
