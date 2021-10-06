1ef1fa2a8ae31dd302c51076ebb404658e596794
Gustavo Garcia <gustavogblanco@live.com>
2021-10-05 19:13:03 -0500

    update type number

---
+++ js/tipoNumber.js
@@ -0,0 +1,11 @@
+var miPrimeraVariable = "Hola a todos";
+/*
+NaN - Not-A-Number
+*/
+console.log(isNaN(miPrimeraVariable));
+miPrimeraVariable = 5;
+console.log(isNaN(miPrimeraVariable));
+//Maximo numero en JS
+console.log(Number.MAX_VALUE);
+//Minimo numero en JS
+console.log(Number.MIN_VALUE);
