55ff2bc247022e6406efcfa9c7dcd661b185336d
Gustavo Garcia <gustavogblanco@live.com>
2021-10-06 20:45:35 -0500

    update matriz

---
+++ js/multidimensionalArrays/multidimensionalArrays.js
@@ -0,0 +1,26 @@
+/*
+* Matrices
+*/
+//Matriz de una dimension
+var unicaDimension = [1,2,3,4,5];
+
+//Matriz de dos dimensiones
+//Matriz de dos dimensiones 2x2
+//Visualmente
+/*
+    0 1
+    2 3
+*/
+var dosDimensiones2x2 = [[0,1], [2,3]];
+console.log(dosDimensiones2x2[1][1]);
+
+//Matriz de dos dimensiones de 3x3
+/*
+    0 1 2
+    3 4 5
+    6 7 8
+*/
+var dosDimensiones3x3 = [[0,1,2], [3,4,5], [6,7,8]];
+console.log(dosDimensiones3x3[1][2]);
+
+
