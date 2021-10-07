c84e7e892f19f6fdf227b8d956e36ffd82b0a233
Gustavo Garcia <gustavogblanco@live.com>
2021-10-06 19:16:32 -0500

    type undefined

---
+++ js/typeUndefined.js
@@ -0,0 +1,10 @@
+//Null y undefined significa ausencia de valor
+console.log(typeof undefined);
+
+var variable;
+console.log(variable);
+
+console.log(null == undefined);
+console.log(null === undefined);
+console.log(1 * undefined);
+console.log(1 * null);
