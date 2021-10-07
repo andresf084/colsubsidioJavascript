4f07ad04073ac4fb1b7f1ed31caf62f290b23078
Gustavo Garcia <gustavogblanco@live.com>
2021-10-06 21:07:03 -0500

    update for

---
+++ js/loops/for/for.js
@@ -0,0 +1,27 @@
+/*
+ Bucle
+ For
+*/
+//For
+//Sintaxis -> for(iterator; condition; end) {code}
+//iterator -> declaracion de una variable y se usa para 
+//inicializar la iteracion
+//condition ->  Se ejecuta el <code> siempre y cuando 
+//la condicion sea verdadera
+//end -> una expresion final que se evalua cada vez que 
+//se ejecuto una iteracion del bucle
+//contador++ -> contador = contador + 1;
+//contador+2 -> contador = contador + 2;
+//contador-4 -> contador = contador - 4;
+for(var contador = 1; contador <= 3; contador++) {
+    console.log(contador);
+}
+
+var ciudades = ['Bucaramanga', 'Cali', 'Bogota', 'Valledupar', 'Barranquilla'];
+
+//Por favor imprimir los valores de las posiciones pares
+//de la siguiente forma
+//'posicion <posicionArreglo> es par y su valor es <valorDelaPosicionDelArreglo>'
+// y los valores de las posiciones impares
+//de la siguiente forma
+//'posicion <posicionArreglo> es impar y su valor es <valorDelaPosicionDelArreglo>'
