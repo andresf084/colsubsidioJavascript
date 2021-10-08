8d86c8f908ad1e97fbdc7cb7531b3bc4c8d31f33
Gustavo Garcia <gustavogblanco@live.com>
2021-10-07 19:35:21 -0500

    update solution

--- js/loops/for/for.js
+++ js/loops/for/for.js
@@ -13,7 +13,7 @@
 //contador++ -> contador = contador + 1;
 //contador+2 -> contador = contador + 2;
 //contador-4 -> contador = contador - 4;
-for(var contador = 1; contador <= 3; contador++) {
+for (var contador = 1; contador <= 3; contador++) {
     console.log(contador);
 }
 
@@ -27,11 +27,11 @@ var ciudades = ['Bucaramanga', 'Cali', 'Bogota', 'Valledupar', 'Barranquilla'];
 //'posicion <posicionArreglo> es impar y su valor es <valorDelaPosicionDelArreglo>'
 //modulo -> %
 //ejemplo de utilizar modulo: 3%2=1; 2%2=0 
-for(var posicionTemporalCiudades = 0; posicionTemporalCiudades < ciudades.length; posicionTemporalCiudades++){
-    if(posicionTemporalCiudades%2==0){
+for (var posicionTemporalCiudades = 0; posicionTemporalCiudades < ciudades.length; posicionTemporalCiudades++) {
+    if (posicionTemporalCiudades % 2 == 0) {
         console.log(`posicion ${posicionTemporalCiudades} es par y su valor es ${ciudades[posicionTemporalCiudades]}`)
     }
-    else if (posicionTemporalCiudades%2==1){
+    else if (posicionTemporalCiudades % 2 == 1) {
         console.log(`posicion ${posicionTemporalCiudades} es impar y su valor es ${ciudades[posicionTemporalCiudades]}`)
     }
 }
@@ -39,7 +39,7 @@ for(var posicionTemporalCiudades = 0; posicionTemporalCiudades < ciudades.length
 /*
 Matriz
 */
-var dosDimensiones3x3 = [[0,1,2], [3,4,5], [6,7,8]];
+var dosDimensiones3x3 = [[0, 1, 2], [3, 4, 5], [6, 7, 8]];
 /*
 Deben utilizar bucles
 suma diagonales
@@ -49,3 +49,18 @@ suma diagonales
 En consola:
 -> La suma de la diagonal es <sumaDiagonales>
 */
+var sumatoria = 0;
+for (var iteratorPrincipal = 0; iteratorPrincipal <= dosDimensiones3x3.length - 1; iteratorPrincipal++) {
+    var matrizInternoTemporal = dosDimensiones3x3[iteratorPrincipal];
+    for (var iteratorSecundario = 0; iteratorSecundario <= matrizInternoTemporal.length - 1; iteratorSecundario++) {
+        if (iteratorPrincipal == iteratorSecundario) {
+            sumatoria = sumatoria + dosDimensiones3x3[iteratorPrincipal][iteratorSecundario];
+        }
+    }
+    for (var iteradorTerciario = matrizInternoTemporal.length - 1; iteradorTerciario >= 0; iteradorTerciario--) {
+        if (iteratorPrincipal + iteradorTerciario == dosDimensiones3x3.length - 1) {
+            sumatoria = sumatoria + dosDimensiones3x3[iteratorPrincipal][iteradorTerciario];
+        }
+    }
+}
+console.log(sumatoria);
