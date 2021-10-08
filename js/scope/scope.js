0fcea17521070e26c446cd7243e035bed007d6c4
Gustavo Garcia <gustavogblanco@live.com>
2021-10-07 21:44:19 -0500

    scope

---
+++ js/scope/scope.js
@@ -0,0 +1,30 @@
+//scope, ambito, alcance
+var variable = 5;
+//console.log(variable);
+
+//De acuerdo al bloque de codigo que lo contiene - local
+for(let contador = 0; contador < 10; contador++) {
+    console.log(contador);
+}
+
+console.log(contador);
+
+//De acuerdo a la funcion que lo contiene - Global
+for(var index = 0; index < 10; index++) {
+    console.log(index);
+}
+
+function otroEjemplo() {
+    var index = 0;
+    console.log(index);
+    function otroEjemplo() {
+        var i = 1;
+        console.log(index);
+    }
+
+    otroEjemplo();
+}
+
+otroEjemplo();
+
+console.log(index);
