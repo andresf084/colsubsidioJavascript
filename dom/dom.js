58917af4f94e1b24967965da99d97e01e0ac3678
Gustavo Garcia <gustavogblanco@live.com>
2021-10-13 18:47:32 -0500

    update DOM

--- dom/dom.js
+++ dom/dom.js
@@ -13,14 +13,30 @@
 //element
 //A un nodo de tipo 'element' que hace parte del DOM
 
-//document.getElementById(<id>)
+//document.getElementById('<id>')
 //Metodo me retorna todo un elemento HTML (nodo) cuyo atributo id es igual al que le pasamos como parametro
 //console.log(document.getElementById('heading'));
 //Para traer contenido usamos la propiedad innerHTML
 //console.log(document.getElementById('heading').innerHTML);
 
-//getElementsByTagName(<name>)
+//getElementsByTagName('<tagName>')
 //Retorna en una lista los elementos con un nombre determinado. Es decir,
-//busca en todo el documento, incluyendo el nodo raiz
+//busca en todo el documento, incluyendo el nodo raiz.
+//Me retorna en una lista(NodeList) los elementos encontrados en el orden en que aparecen en el árbol.
 const parrafos = document.getElementsByTagName('p');
-console.log(parrafos);
+//console.log(parrafos);
+
+//createElement('<tagName>', [options])
+//Crea un elemento HTML especifico por su tagName
+//Parametros
+//<tagName> -> Especifica el tipo de elemento a crear. Por ende, el nodeName
+//del elemento es creado con el valor de tagName
+//[options] -> contiene una única propiedad llamada is, cuyo valor es el de la etiqueta
+//tagName de un elemento personalizado.
+const parrafo = document.createElement('p');
+console.log(parrafo);
+
+//appendChild('<child>');
+//Agrega un nuevo nodo al final de la lista de un elemento hijo de un elemento padre especifico
+//Child -> hijo y es una referencia hacia un nodo existente en el documento actual
+document.body.appendChild(parrafo);
